3651ac112d3ccdb32edf33b62c0822202510b995
Wani Joseph <waynekhalifa.wk@gmail.com>
2022-10-09 11:09:59 +0200

    adds bubles

--- src/components/Survey/Survey.tsx
+++ src/components/Survey/Survey.tsx
@@ -47,7 +47,70 @@ const Survey: React.FC = () => {
         </Box>
       </Grid>
       <Grid item xs={12} md={3}>
-        <Box sx={{ backgroundColor: "primary.light", height: "100vh" }} />
+        <Box
+          sx={{
+            backgroundColor: "primary.light",
+            height: "100vh",
+            position: "relative",
+            overflow: "hidden",
+          }}
+        >
+          <Box
+            sx={{
+              backgroundColor: "primary.main",
+              position: "absolute",
+              width: "30px",
+              height: "30px",
+              borderRadius: "30px",
+              top: "30px",
+              right: "60px",
+            }}
+          />
+          <Box
+            sx={{
+              backgroundColor: "primary.main",
+              position: "absolute",
+              width: "250px",
+              height: "250px",
+              borderRadius: "250px",
+              top: "60px",
+              left: "-125px",
+            }}
+          />
+          <Box
+            sx={{
+              backgroundColor: "primary.main",
+              position: "absolute",
+              width: "25px",
+              height: "25px",
+              borderRadius: "25px",
+              top: "350px",
+              left: "30px",
+            }}
+          />
+          <Box
+            sx={{
+              backgroundColor: "primary.main",
+              position: "absolute",
+              width: "15px",
+              height: "15px",
+              borderRadius: "15px",
+              bottom: "350px",
+              right: "40px",
+            }}
+          />
+          <Box
+            sx={{
+              backgroundColor: "primary.main",
+              position: "absolute",
+              width: "130px",
+              height: "130px",
+              borderRadius: "130px",
+              bottom: "150px",
+              right: "-65px",
+            }}
+          />
+        </Box>
       </Grid>
     </Grid>
   );
