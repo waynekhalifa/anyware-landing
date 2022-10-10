f8342f8354f69aaf29c9c3f6c7e25309c94e2aca
Wani Joseph <waynekhalifa.wk@gmail.com>
2022-10-10 12:42:58 +0200

    adds video placeholder

--- src/components/Featured/Video.tsx
+++ src/components/Featured/Video.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+import { Box } from "@mui/material";
+
+import videoPlaceholder from "@images/banner.svg";
+
 interface VideoProps {}
 
 const Video: React.FC<VideoProps> = ({}) => {
-  return <>Video Placeholder...</>;
+  return (
+    <Box sx={{ position: "relative", zIndex: 10 }}>
+      <Image src={videoPlaceholder} />
+    </Box>
+  );
 };
 
 export default Video;
