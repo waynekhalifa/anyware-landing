cd8da6580b517e839b98454276f9409e77a0749b
Wani Joseph <waynekhalifa.wk@gmail.com>
2022-10-11 16:23:28 +0200

    updates menu items

--- src/components/HomePage/MainNavigation.tsx
+++ src/components/HomePage/MainNavigation.tsx
@@ -1,46 +1,35 @@
+import useMenu from "@/hooks/useMenu";
+import NavigationDropDown from "./NavigationDropDown";
 import NavigationItem from "./NavigationItem";
 
 const MainNavigation: React.FC = () => {
+  const { menusListing } = useMenu("menus", "menu");
+
   return (
     <nav className="nav navigation__menu" aria-label="Main Navigation">
       <ul id="navigation" className="navigation__list" role="list">
-        <NavigationItem />
+        {menusListing.map((item: any) => {
+          return item.children.length > 0 ? (
+            <NavigationDropDown key={item.id} />
+          ) : (
+            <NavigationItem key={item.id} item={item} />
+          );
+        })}
       </ul>
-      <div className="navigation__auth">
+      <div className="navigation__auth-button-group">
         <button
-          className="navigation__link axiforma"
+          className="navigation__auth-item auth__item auth__item_trial axiforma"
           type="button"
-          data-modal="enterprise"
+          data-beta
           data-ga-click-tracking
-          ga-event="Enterprise quote click"
-          ga-category="Enterprise quote button"
+          lp-plan="free-forever"
+          ga-event="Free forever click"
+          ga-category="Free forever button"
+          ga-label="menu button"
+          mail-label="menu free forever"
         >
           Contact Sales
         </button>
-        <div className="navigation__auth-button-group">
-          <button
-            className="navigation__auth-item auth__item auth__item_trial axiforma"
-            type="button"
-            data-beta
-            data-ga-click-tracking
-            lp-plan="free-forever"
-            ga-event="Free forever click"
-            ga-category="Free forever button"
-            ga-label="menu button"
-            mail-label="menu free forever"
-          >
-            Sign Up
-          </button>
-          <a
-            className="navigation__auth-item auth__item auth__item_sign-in axiforma"
-            href="https://app.clickup.com"
-            data-ga-click-tracking
-            ga-event="Landing page SignIn button"
-            ga-category="Landing page SignIn button"
-          >
-            Log in
-          </a>
-        </div>
       </div>
     </nav>
   );
