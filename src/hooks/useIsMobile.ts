f2e7158df23adb01fdbecf0bd45b51a98b58b243
hazemheagzy1 <Hazem.Alhegazy@gmail.com>
2023-07-17 13:40:29 +0300

    ismobile

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -1,21 +1,40 @@
-import React, { useEffect, useLayoutEffect,useState } from 'react'
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 
 const useIsMobile = () => {
-    const [width, setWidth] = useState<number>(window.innerWidth);
-    
-    function handleWindowSizeChange() {
-        setWidth(window.innerWidth);
+  const router = useRouter();
+  const [width, setWidth] = useState<number>(-1);
+  const [windowLoaded, setWindowLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    console.log("ho")
+    const handleWindowSizeChange = () => {
+      setWidth(window.innerWidth);
+    };
+
+    const handleRouteChangeComplete = () => {
+      setWindowLoaded(true);
+    };
+
+    if (typeof window !== "undefined") {
+      setWidth(window.innerWidth);
+      setWindowLoaded(true);
+
+      window.addEventListener("resize", handleWindowSizeChange);
+      router.events.on("routeChangeComplete", handleRouteChangeComplete);
     }
-    useEffect(() => {
-        window.addEventListener('resize', handleWindowSizeChange);
-        return () => {
-            window.removeEventListener('resize', handleWindowSizeChange);
-        }
-    }, [window.innerWidth]);
-    
-    const isMobile = width <= 768;
-    return{isMobile,width}
-}
 
+    return () => {
+      if (typeof window !== "undefined") {
+        window.removeEventListener("resize", handleWindowSizeChange);
+        router.events.off("routeChangeComplete", handleRouteChangeComplete);
+      }
+    };
+  }, [router.events]);
 
-export default useIsMobile
+  const isMobile = width <= 768;
+
+  return { windowLoaded, isMobile, width };
+};
+
+export default useIsMobile;
